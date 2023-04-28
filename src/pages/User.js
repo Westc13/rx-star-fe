36527d320fc355eb5a4079e7a7c006abf44a2291
ChristineWest <christinewwwest@gmail.com>
2023-04-28 11:04:47 -0400

    set up a form in User to capture user info

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const User = () => {
-  return <div>User</div>;
+  return (
+    <div>
+      <h1>User</h1>
+      <form action="">
+        <label htmlFor="age">Age</label>
+        <input type="number" />
+        <label htmlFor="gender">Gender</label>
+        <input type="text" />
+        <label htmlFor="conditions">Medical Conditions</label>
+        <input type="text" />
+        <label htmlFor="allergies">Allergies</label>
+        <input type="text" />
+      </form>
+    </div>
+  );
 };
 
 export default User;
